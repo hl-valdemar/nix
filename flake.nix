2185c85451f45a5822adc34bfdd81e974633c63d
hl-valdemar <valdemar.lorenzen@gmail.com>
2025-02-11 12:14:37 +0100

    Reorganized

--- flake.nix
+++ flake.nix
@@ -24,7 +24,30 @@
     mac-app-util,
   }: let
     # System configuration modules
-    systemConfig = import ./modules/system/config.nix;
+    systemConfig = {
+      nixpkgs = {
+        config.allowUnfree = true;
+        hostPlatform = "aarch64-darwin";
+      };
+
+      nix.settings.experimental-features = "nix-command flakes";
+      nix.settings.trusted-users = ["root" "valdemar"];
+
+      system = {
+        configurationRevision = self.rev or self.dirtyRev or null;
+        stateVersion = 6;
+
+        activationScripts.preActivation.text = ''
+          echo "Installing Rosetta..." >&2
+          softwareupdate --install-rosetta --agree-to-license
+        '';
+      };
+
+      users.users.valdemar = {
+        name = "valdemar";
+        home = "/Users/valdemar";
+      };
+    };
 
     # Package management configuration
     packagesConfig = import ./modules/system/packages.nix;
