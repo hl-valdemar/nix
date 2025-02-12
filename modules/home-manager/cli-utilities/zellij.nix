3d37abecc49630edeec807470d044b8a1c29c51b
hl-valdemar <valdemar.lorenzen@gmail.com>
2025-02-12 10:58:19 +0100

    Added a theme to zellij

--- modules/home-manager/cli-utilities/zellij.nix
+++ modules/home-manager/cli-utilities/zellij.nix
@@ -1,7 +1,17 @@
 {
+  xdg.configFile = {
+    "zellij/themes/gruvbox.kdl" = {
+      source = ./zellij/themes/gruvbox.kdl;
+    };
+  };
+
   programs.zellij = {
     enable = true;
     enableZshIntegration = false;
     enableBashIntegration = false;
+
+    settings = {
+      theme = "gruvbox-dark";
+    };
   };
 }
