0bd4d7d01e82a9c30a781a0f4a426d6d8d2ea9d9
hl-valdemar <valdemar.lorenzen@gmail.com>
2025-02-13 16:06:04 +0100

    Added Messenger to the list of mas apps

--- modules/homebrew/default.nix
+++ modules/homebrew/default.nix
@@ -1,12 +1,16 @@
 {
   homebrew = {
     enable = true;
+
     brews = ["mas"];
     casks = ["brave-browser"];
+
     masApps = {
       "Craft" = 1487937127;
       "Perplexity" = 6714467650;
+      "Messenger" = 1480068668;
     };
+
     onActivation = {
       cleanup = "zap";
       autoUpdate = true;
