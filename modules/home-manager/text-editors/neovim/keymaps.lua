4af57650a2cb458cf4ddcfcf6b1354dba5ad8b49
hl-valdemar <valdemar.lorenzen@gmail.com>
2025-04-03 09:35:40 +0200

    Added toggle-enabled terminal scratchpad overlay

--- modules/home-manager/text-editors/neovim/keymaps.lua
+++ modules/home-manager/text-editors/neovim/keymaps.lua
@@ -1,23 +1,23 @@
--- Stop highlighting
+-- stop highlighting
 vim.keymap.set("n", "<esc>", vim.cmd.noh)
 
--- Window navigation
+-- window navigation
 vim.keymap.set("n", "<C-h>", "<C-w>h", { silent = true })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { silent = true })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { silent = true })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { silent = true })
 
--- Toggle file tree/explorer
+-- toggle file tree/explorer
 vim.keymap.set("n", "<leader>v", vim.cmd.NvimTreeToggle)
 
--- Telescope keymaps
+-- telescope keymaps
 local telescope = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", telescope.find_files, { desc = "Telescope find files" })
 vim.keymap.set("n", "<leader>fg", telescope.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", telescope.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>fh", telescope.help_tags, { desc = "Telescope help tags" })
 
--- Flash movement
+-- flash movement
 local flash = require("flash")
 vim.keymap.set({ "n", "x", "o" }, "s", flash.jump, { desc = "Flash" })
 vim.keymap.set({ "n", "x", "o" }, "S", flash.treesitter, { desc = "Flash Treesitter" })
@@ -25,7 +25,7 @@ vim.keymap.set({ "o" }, "r", flash.remote, { desc = "Remote Flash" })
 vim.keymap.set({ "o", "x" }, "R", flash.treesitter_search, { desc = "Treesitter Search" })
 vim.keymap.set({ "c" }, "<c-s>", flash.toggle, { desc = "Toggle Flash Search" })
 
--- TreeSJ
+-- treesj
 local treesj = require("treesj")
 vim.keymap.set("n", "<leader>s", treesj.split, { desc = "TreeSJ split" })
 vim.keymap.set("n", "<leader>j", treesj.join, { desc = "TreeSJ join" })
--- modules/home-manager/text-editors/neovim/options.lua
+++ modules/home-manager/text-editors/neovim/options.lua
@@ -60,3 +60,50 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 	pattern = "*.mqh",
 	command = "set filetype=cpp",
 })
+
+-- Create the toggle function in the global scope for easier access
+function _G.toggle_float_term()
+	-- Check if we already have a terminal window reference
+	if vim.g.term_win and vim.api.nvim_win_is_valid(vim.g.term_win) then
+		-- Close the window if it exists
+		vim.api.nvim_win_close(vim.g.term_win, true)
+		vim.g.term_win = nil
+	else
+		-- Calculate dimensions
+		local width = math.floor(vim.o.columns * 0.8)
+		local height = math.floor(vim.o.lines * 0.8)
+		local col = math.floor((vim.o.columns - width) / 2)
+		local row = math.floor((vim.o.lines - height) / 2)
+
+		-- Create window options
+		local opts = {
+			relative = "editor",
+			width = width,
+			height = height,
+			col = col,
+			row = row,
+			style = "minimal",
+			border = "double",
+		}
+
+		-- Create or reuse the buffer
+		if not vim.g.term_buf or not vim.api.nvim_buf_is_valid(vim.g.term_buf) then
+			vim.g.term_buf = vim.api.nvim_create_buf(false, true)
+		end
+
+		-- Create the window and open terminal
+		vim.g.term_win = vim.api.nvim_open_win(vim.g.term_buf, true, opts)
+
+		-- Only start terminal if it's not already one
+		if vim.bo[vim.g.term_buf].buftype ~= "terminal" then
+			vim.fn.termopen(vim.o.shell)
+			vim.cmd("startinsert")
+		end
+	end
+end
+
+-- Map leader+f to the global function
+vim.api.nvim_set_keymap("n", "<leader>t", "<cmd>lua _G.toggle_float_term()<CR>", { noremap = true, silent = true })
+
+-- Use Shift+Escape to exit terminal mode
+vim.api.nvim_set_keymap("t", "<S-Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
