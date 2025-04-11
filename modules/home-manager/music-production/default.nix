937125748df6cfe2aca9a2983eb93290cf67b9e1
hl-valdemar <valdemar.lorenzen@gmail.com>
2025-04-11 21:22:28 +0200

    Moved sunvox to be managed by homebrew hook

--- modules/home-manager/music-production/default.nix
+++ modules/home-manager/music-production/default.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [
-    sunvox
-  ];
+  # home.packages = with pkgs; [
+  #   sunvox
+  # ];
 }
--- modules/homebrew/default.nix
+++ modules/homebrew/default.nix
@@ -11,6 +11,7 @@
       "signal"
       "raycast"
       "docker"
+      "sunvox"
     ];
 
     masApps = {
