d1287750bff564b00138810640283464cea11084
hl-valdemar <valdemar.lorenzen@gmail.com>
2025-06-25 14:43:43 +0200

    Further configured macOS system settings
    Specifically, the dock and finder.

--- modules/macos/config.nix
+++ modules/macos/config.nix
@@ -7,10 +7,16 @@
         "/System/Applications/Calendar.app"
         "/Applications/Claude.app"
       ];
+      mineffect = "scale";
+      show-recents = false;
+      tilesize = 32;
     };
 
     # “icnv” = Icon view, “Nlsv” = List view, “clmv” = Column View, “Flwv” = Gallery View
-    finder.FXPreferredViewStyle = "clmv";
+    finder = {
+      FXPreferredViewStyle = "clmv";
+      AppleShowAllExtensions = true;
+    };
 
     loginwindow.GuestEnabled = false;
     NSGlobalDomain.KeyRepeat = 2;
